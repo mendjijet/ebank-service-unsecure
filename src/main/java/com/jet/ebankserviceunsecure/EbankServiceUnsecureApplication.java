98a9f7f9c08eeec5322b891e39224eb19f4e3cdf
mendjijet <mendjijet@gmail.com>
2023-03-11 07:20:06 +0100

    first commit

---
+++ src/main/java/com/jet/ebankserviceunsecure/EbankServiceUnsecureApplication.java
@@ -0,0 +1,13 @@
+package com.jet.ebankserviceunsecure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbankServiceUnsecureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbankServiceUnsecureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jet/ebankserviceunsecure/EbankServiceUnsecureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jet.ebankserviceunsecure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbankServiceUnsecureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
